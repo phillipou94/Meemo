8953a1c7d2b5c4a627c798bf9c4fa3b1c0d302e5
Phillip Ou <pou@mit.edu>
2015-04-24 00:09:57 -0700

    working on image loading

--- Meemo/CreateGroupsViewController.swift
+++ Meemo/CreateGroupsViewController.swift
@@ -7,20 +7,113 @@
 //
 
 import Foundation
+import Spring
+import Photos
 
-class CreateGroupsViewController: UIViewController {
+class CreateGroupsViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var headerView: UIView!
     @IBOutlet var groupTextField: UITextField!
+    @IBOutlet var photoContainer: SpringView!
+    @IBOutlet var groupNameLabel: UILabel!
+    @IBOutlet var messageView: SpringView!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
         UIApplication.sharedApplication().setStatusBarHidden(true, withAnimation: .None)
         
         let shadeView = ShadeView(frame: self.view.frame)
         self.view.insertSubview(shadeView, belowSubview: headerView)
+        
+        setUpTextField()
+        fetchPhotos()
     }
     
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
+    
+    func setUpTextField() {
+        let color = UIColor.whiteColor().colorWithAlphaComponent(0.6)
+        self.groupTextField.attributedPlaceholder = NSAttributedString(string: "Name Your Group", attributes: [NSForegroundColorAttributeName:color])
+        
+        self.groupTextField.delegate = self
+        
+    }
+    @IBAction func checkPressed(sender: AnyObject) {
+        
+        if (self.groupTextField.text.length > 0) {
+            self.groupNameLabel.text = self.groupTextField.text
+            self.groupTextField.text = "Choose a Photo"
+            self.groupTextField.userInteractionEnabled = false
+            animatePhotoContainerTransition()
+        }
+        
+    }
+    
+    var images:NSMutableArray! // <-- Array to hold the fetched images
+    
+    func fetchPhotos () {
+        images = NSMutableArray()
+        self.fetchPhotoAtIndexFromEnd(0)
+    }
+    
+    // Repeatedly call the following method while incrementing
+    // the index until all the photos are fetched
+    func fetchPhotoAtIndexFromEnd(index:Int) {
+        
+        let imgManager = PHImageManager.defaultManager()
+        
+        // Note that if the request is not set to synchronous
+        // the requestImageForAsset will return both the image
+        // and thumbnail; by setting synchronous to true it
+        // will return just the thumbnail
+        var requestOptions = PHImageRequestOptions()
+        requestOptions.synchronous = true
+        
+        // Sort the images by creation date
+        var fetchOptions = PHFetchOptions()
+        fetchOptions.sortDescriptors = [NSSortDescriptor(key:"creationDate", ascending: true)]
+        
+        if let fetchResult = PHAsset.fetchAssetsWithMediaType(PHAssetMediaType.Image, options: fetchOptions) {
+            let asset = fetchResult.objectAtIndex(1) as! PHAsset
+            loadImageFrom(asset, completionHandler: { (image) -> Void in
+                let a = image
+                
+                let em = 5.0
+           
+            })
+            
+
+        }
+    }
+    
+    func loadImageFrom(asset:PHAsset,completionHandler:(image:UIImage) -> Void) {
+        let imgManager = PHImageManager.defaultManager()
+        var requestOptions = PHImageRequestOptions()
+        requestOptions.synchronous = true
+        
+        imgManager.requestImageForAsset(asset, targetSize: view.frame.size, contentMode: PHImageContentMode.AspectFill, options: requestOptions, resultHandler: { (image, _) in
+            
+            completionHandler(image:image)
+        })
+    }
+    
+    
+    
+
+    
+    func animatePhotoContainerTransition() {
+        self.messageView.animation = "slideRight"
+        self.messageView.animateToNext({ () -> () in
+            self.messageView.layer.frame.origin.x = -600
+        })
+        
+        self.photoContainer.animation = "slideLeft"
+        self.photoContainer.animate()
+        
+    }
+
 
 }
