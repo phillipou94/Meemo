b40ca44661fbf03916f7578183ddb566ca85b190
Phillip Ou <pou@mit.edu>
2015-05-01 13:33:44 -0700

    finished laying out phone number

--- Meemo/PhoneSearchViewController.swift
+++ Meemo/PhoneSearchViewController.swift
@@ -8,13 +8,23 @@
 
 import UIKit
 
-class PhoneSearchViewController: UIViewController {
+class PhoneSearchViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var phoneTextField: UITextField!
+    @IBOutlet weak var backSpaceButton: UIButton!
+    var phoneNumber = ""
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.phoneTextField.delegate = self;
+        
+        let bottomBorder = CALayer()
+        bottomBorder.frame = CGRectMake(0,self.phoneTextField.frame.size.height+30, self.phoneTextField.frame.size.width,2.0)
+        bottomBorder.backgroundColor = UIColor(red: 63/255.0, green: 61/255.0, blue: 82/255.0, alpha: 1.0).CGColor
+        self.phoneTextField.layer.addSublayer(bottomBorder)
+    }
+    
+    func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
+        return false
     }
     
     override init(nibName nibNameOrNil: String!, bundle nibBundleOrNil: NSBundle!) {
@@ -30,7 +40,76 @@ class PhoneSearchViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func backSpacePressed(sender: AnyObject) {
+        if phoneNumber.length > 0 {
+            let lastIndex = phoneNumber.length - 1
+            phoneNumber = phoneNumber.substringToIndex(advance(phoneNumber.startIndex, lastIndex))
+            phoneTextField.text = phoneNumber
+        }
+        
+    }
 
+    @IBAction func onePressed(sender: AnyObject) {
+        formatPhoneString("1")
+    }
+
+    @IBAction func twoPressed(sender: AnyObject) {
+        formatPhoneString("2")
+    }
+    
+    @IBAction func threePressed(sender: AnyObject) {
+        formatPhoneString("3")
+    }
+    @IBAction func fourPressed(sender: AnyObject) {
+        formatPhoneString("4")
+    }
+    
+    @IBAction func fivePressed(sender: AnyObject) {
+        formatPhoneString("5")
+    }
+    
+    @IBAction func sixPressed(sender: AnyObject) {
+        formatPhoneString("6")
+    }
+    
+    @IBAction func sevenPressed(sender: AnyObject) {
+        formatPhoneString("7")
+    }
+    
+    @IBAction func eightPressed(sender: AnyObject) {
+        formatPhoneString("8")
+    }
+    
+    @IBAction func ninePressed(sender: AnyObject) {
+        formatPhoneString("9")
+    }
+    
+    @IBAction func zeroPressed(sender: AnyObject) {
+        formatPhoneString("0")
+    }
+    
+    func formatPhoneString(number:String) {
+        if phoneNumber.length < 14 {
+            if phoneNumber.length == 0 {
+                phoneNumber += "("
+            }
+            phoneNumber += number
+            if phoneNumber.length == 4 {
+                phoneNumber += ") "
+            }
+            if phoneNumber.length == 9 {
+                phoneNumber += "-"
+            }
+            
+            phoneTextField.text = phoneNumber
+        }
+        
+        
+    }
+    
+    
+    
+    
     /*
     // MARK: - Navigation
 
