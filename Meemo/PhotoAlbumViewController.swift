29511a149386184bf57c9f8f339199781a63d077
Phillip Ou <pou@mit.edu>
2015-05-15 00:12:23 -0700

    finished configuring album view

--- Meemo/PhotoAlbumViewController.swift
+++ Meemo/PhotoAlbumViewController.swift
@@ -9,34 +9,109 @@
 import UIKit
 import Photos
 
-class PhotoAlbumViewController: UIViewController, UIScrollViewDelegate {
+class PhotoAlbumViewController: UIViewController, UIScrollViewDelegate, UICollectionViewDelegate, UICollectionViewDataSource {
 
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var scrollView: UIScrollView!
     var images:PHFetchResult = PHFetchResult()
+    var collectionViewContainer =  UIView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpScrollView()
         fetchPhotos()
         
+        
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        setUpCollectionView()
     }
     
     @IBAction func exitPressed(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
+    //MARK: - CollectionView
+    
+    func setUpCollectionView() {
+       
+
+        
+        self.collectionViewContainer.frame = CGRectMake(0,self.scrollView.frame.origin.y+self.scrollView.frame.size.height,self.view.frame.size.width,self.view.frame.size.height - 40)
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 1, left: 1, bottom: 1, right: 1)
+        let frame = CGRectMake(0, 40, self.view.frame.size.width, self.collectionViewContainer.frame.size.height - 40)
+        var collectionView = UICollectionView(frame: frame, collectionViewLayout: layout)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.registerNib(UINib(nibName: "PhotoCell", bundle: nil), forCellWithReuseIdentifier: "PhotoCell")
+
+        self.collectionViewContainer.addSubview(collectionView)
+        let pan = UIPanGestureRecognizer(target: self, action: "draggedCollectionView:")
+        //self.collectionViewContainer.addGestureRecognizer(pan)
+        collectionView.alwaysBounceVertical = true
+        
+        self.view.addSubview(self.collectionViewContainer)
+        self.collectionViewContainer.bringSubviewToFront(collectionView)
+    }
+    
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.images.count+1;
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        let width = self.view.frame.size.width/3 - 8
+        return CGSize(width: width, height: width)
+    }
+    
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+        return UIEdgeInsetsMake(1, 1, 1, 1)
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PhotoCell", forIndexPath: indexPath) as! PhotoCell
+        if (indexPath.row == 0) {
+            let camera = UIImage(named: "camera_L")
+            cell.imageView.hidden = true
+            cell.accessoryView.image = camera
+            cell.accessoryView.hidden = false
+            cell.imageView.backgroundColor = UIColor.redColor()
+        } else{
+            let asset = self.images[indexPath.row-1] as! PHAsset
+            loadThumbnailFrom(asset, completionHandler: { (image) -> Void in
+                let thumbnail: UIImage = image as UIImage
+                cell.imageView.image = thumbnail
+                cell.imageView.hidden = false
+                cell.accessoryView.hidden = true
+            })
+            /*let checkmark = UIImage(named:"Checkmark")
+            cell.accessoryView.image = checkmark
+            if indexPath.row != selectedIndex {
+                cell.accessoryView.hidden = true
+            } else {
+                cell.accessoryView.hidden = false
+            }*/
+        }
+        
+        
+        return cell
+    }
+
+    
     
     //MARK: - ALAssets
     
     func fetchPhotos () {
         let imgManager = PHImageManager.defaultManager()
-        
-        // Note that if the request is not set to synchronous
-        // the requestImageForAsset will return both the image
-        // and thumbnail; by setting synchronous to true it
-        // will return just the thumbnail
         var requestOptions = PHImageRequestOptions()
         requestOptions.synchronous = true
         
@@ -75,13 +150,13 @@ class PhotoAlbumViewController: UIViewController, UIScrollViewDelegate {
         })
     }
     
-    func loadThumNailFrom(asset:PHAsset,completionHandler:(image:UIImage) -> Void) {
+    func loadThumbnailFrom(asset:PHAsset,completionHandler:(image:UIImage) -> Void) {
         let imgManager = PHImageManager.defaultManager()
         var requestOptions = PHImageRequestOptions()
         requestOptions.synchronous = true
         
         imgManager.requestImageForAsset(asset,
-            targetSize: CGSizeMake(100.0,100.0),
+            targetSize: CGSizeMake(200.0,200.0),
             contentMode: PHImageContentMode.AspectFit,
             options: requestOptions,
             resultHandler: { (image, _) in
